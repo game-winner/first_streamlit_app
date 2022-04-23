03226f431f0c6ad905d3ec471bcdf03a2807aa0c
game-winner <dady.coulibaly@outlook.com>
2022-04-23 15:21:07 -0400

    Update streamlit_app.py
    added preselected fruits to the picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,6 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #pick list to let user select fruits from the list below
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 streamlit.dataframe(my_fruit_list)
