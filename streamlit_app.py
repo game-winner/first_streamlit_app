69db0dcb2f3d2386fdd4731f11f1c8579fa375ab
game-winner <dady.coulibaly@outlook.com>
2022-04-27 13:19:42 -0400

    Added call to fruityvice.com API
    Displayed response in new section

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 
 import streamlit
 import pandas
+import requests
 
 streamlit.title('My Parents New Healthy Diner!')
 
@@ -20,3 +21,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
 streamlit.dataframe(fruits_to_show)
+
+streamlit.header('FruityVice Fruit Advice')
+
+fruityvice_response = requests.get("fruityvice.com/api/fruit/watermelon")
+
+streamlit.text(fruityvice_response.json())
