48b9266b86ca1d721c4d5862b4886baa1f511376
game-winner <dady.coulibaly@outlook.com>
2022-04-30 12:59:10 -0400

    imported urlerror library
    added stop line

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLerror
 
 streamlit.title('My Parents New Healthy Diner!')
 
@@ -33,6 +34,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
 #snowflake query
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
