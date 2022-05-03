cb64caff18f23ec57f9bba12559ed1da6914ac2a
game-winner <dady.coulibaly@outlook.com>
2022-05-02 23:38:41 -0400

    added error handling for control flow

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,15 +25,18 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('FruityVice Fruit Advice')
-fruit_choice = streamlit.text_input("What fruit would you like advice about?", "Kiwi")
-streamlit.write("You have entered: " + fruit_choice)
+try:
+  fruit_choice = streamlit.text_input("What fruit would you like advice about?")
+  if not fruit_choice:
+    streamlit.error("please pick a fruit to obtain information")
+  else:
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  streamlit.dataframe(fruityvice_normalized)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-streamlit.dataframe(fruityvice_normalized)
-
+except URLError as e:
+  streamlit.error()
+  
 streamlit.stop()
 #snowflake query
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
