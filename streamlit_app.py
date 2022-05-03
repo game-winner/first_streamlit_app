46794e50a4cae78de370b5a80ac0815e5fdba51e
game-winner <dady.coulibaly@outlook.com>
2022-05-02 23:58:46 -0400

    wip

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('FruityVice Fruit Advice')
-def get_fruityvice_data():
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
   
