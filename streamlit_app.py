4419c1c3f9859f66d39550c91d2c355f6c4f9486
game-winner <dady.coulibaly@outlook.com>
2022-05-03 22:45:32 -0400

    added insert to snowflake with button

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,6 +57,15 @@ if streamlit.button("Get Fruit Load List"):
     streamlit.dataframe(my_data_rows)
     
 fruit_add = streamlit.text_input("What fruit would you like to add?", "Banana")
-streamlit.write("Thanks for adding " + fruit_add)
 
-#my_cur.execute("insert into fruit_load_list values ('from streamlit') ")
+def insert_row_snowflake(fruit_add):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values (my_fruit) ")
+        return "Thanks for adding " + fruit_add
+
+if streamlit.button("Add a fruit to the list"):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(fruit)
+    streamlit.text(back_from_function)
+    
+    
